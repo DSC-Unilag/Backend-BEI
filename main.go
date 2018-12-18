db3450f206963bbfd56c62dd5a5861a79cc8245e
Uti Michael <utimichael9@gmail.com>
2018-12-18 13:06:27 +0100

    Update main.go

--- main.go
+++ main.go
@@ -9,18 +9,17 @@ import (
 func main() {
 	var user db.User
 	user = db.User{
-		Name: "Uti Michael",
+		Name: "Test
 	}
-<<<<<<< HEAD
-	tobi := db.User{
-		Name: "Oduah Tobi",
+	test1 := db.User{
+		Name: "Test1",
 	}
-	egahi := db.User{
-		Name: "Egahi Okwori",
+	test2 := db.User{
+		Name: "Test2",
 	}
 	user.Create()
-	tobi.Create()
-	egahi.Create()
+	test1.Create()
+	test2.Create()
 	fmt.Println(user)
 	var questions db.Question
 	questions = db.Question{
@@ -32,8 +31,8 @@ func main() {
 	fmt.Println(questions)
 	var comment db.Comment
 	comment = db.Comment{
-		Author:     tobi,
-		UserID:     tobi.ID,
+		Author:     test1,
+		UserID:     test1.ID,
 		QuestionID: questions.ID,
 		Content:    "Hi there",
 	}
@@ -47,9 +46,9 @@ func main() {
 	comment.Create()
 	var reply db.Reply
 	reply = db.Reply{
-		Author:     egahi,
+		Author:     test1,
 		Content:    "Hello,",
-		UserID:     egahi.ID,
+		UserID:     test1.ID,
 		QuestionID: questions.ID,
 		CommentID:  comment.ID,
 	}
@@ -65,61 +64,6 @@ func main() {
 	fmt.Println(questions)
 	fmt.Println(comment)
 	fmt.Println(reply)
-=======
-	// tobi := db.User{
-	// 	Name: "Oduah Tobi",
-	// }
-	// egahi := db.User{
-	// 	Name: "Egahi Okwori",
-	// }
-	// user.Create()
-	// tobi.Create()
-	// egahi.Create()
-	// fmt.Println(user)
-	// var questions db.Question
-	// questions = db.Question{
-	// 	Author:  user,
-	// 	UserID:  user.ID,
-	// 	Content: "Hello All",
-	// }
-	// questions.Create()
-	// fmt.Println(questions)
-	// var comment db.Comment
-	// comment = db.Comment{
-	// 	Author:     tobi,
-	// 	UserID:     tobi.ID,
-	// 	QuestionID: questions.ID,
-	// 	Content:    "Hi there",
-	// }
-	// comment.Create()
-	// comment = db.Comment{
-	// 	Author:     user,
-	// 	UserID:     user.ID,
-	// 	QuestionID: questions.ID,
-	// 	Content:    "Hi there",
-	// }
-	// comment.Create()
-	// var reply db.Reply
-	// reply = db.Reply{
-	// 	Author:     egahi,
-	// 	Content:    "Hello,",
-	// 	UserID:     egahi.ID,
-	// 	QuestionID: questions.ID,
-	// 	CommentID:  comment.ID,
-	// }
-	// reply.Create()
-	// comment = db.Comment{
-	// 	Author:     user,
-	// 	UserID:     user.ID,
-	// 	QuestionID: questions.ID,
-	// 	Content:    "I am Michael Uti",
-	// }
-	// comment.Create()
-	// fmt.Println(user)
-	// fmt.Println(questions)
-	// fmt.Println(comment)
-	// fmt.Println(reply)
->>>>>>> 12f3627b6753a667b7e8f630463e9a4160e24459
 	user.Retrieve()
 	fmt.Println(user)
 	fmt.Println("Questions from", user.Name, "are: ")
